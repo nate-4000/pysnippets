aa68bbebf930500f5e2afe9434a21c249ec2af7d
nate-4000 <83925246+nate-4000@users.noreply.github.com>
2022-06-24 13:43:12 -0700

    Update main.py

--- PRAN searcher/main.py
+++ PRAN searcher/main.py
@@ -1,7 +1,7 @@
 import pran
 import time
-s = float("0." + input("Input a float under one. 0."))
-d = bool(int(input("Do you want progress info? (can spam, use 0 for no and 1 for yes) ")))
+s = ("0." + input("Input a float under one. 0."))
+d = bool(int(input("Do you want progress info? (use 0 for no and 1 for yes) ")))
 i = 0
 j = 0
 cs = pran.snrr(0)
@@ -10,7 +10,7 @@ while not str(cs).__contains__(str(s)):
     i = i + 1
     cs = pran.snrr(i)
     if d:
-        print(str(i) + " " + str(cs))
+        print(str(i) + " " + str(cs) + "\r", end="", flush=True)
     else:
         j = j + 1
         if j == 16:
@@ -18,4 +18,4 @@ while not str(cs).__contains__(str(s)):
         print("Calculating..." + spin[int((j/4)%4)],end="\r",flush=True)
 print()
 print("The seed containing " + str(s) + " is equal to " + str(i) + ".")
-input("Press any key to exit...")
+input("Press enter to exit...")
